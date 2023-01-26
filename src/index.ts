4cf7708d118fb870b19c487c9286bfe46a4214bc
hung222 <90371118+hung222@users.noreply.github.com>
2023-01-26 21:42:56 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.raw({ type: "application/vnd.custom-type" }));
 app.use(express.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ message: "Please visit /countries to view all the countries" });
+  res.json({ message: "cực mê giác hơi muốn gh 100 hu3 quá" });
 });
 
 app.use("/countries", countryRoutes);
@@ -18,3 +18,4 @@ app.use("/countries", countryRoutes);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
